df2a83da3253b263e5d04d638bd2a1b19c675489
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-13 22:45:42 +0530

    nav for phone done

--- starter-template/src/app/page.tsx
+++ starter-template/src/app/page.tsx
@@ -1,7 +1,7 @@
+import Header from "@/sections/Header";
+
 export default function Home() {
-  return <h1 className="text-5xl">I write code,  
-I ship fast.  
-From stack to chain,  
-I build it all.  
-</h1>;
+  return <>
+    <Header/>
+  </> 
 }
--- starter-template/src/sections/Header.tsx
+++ starter-template/src/sections/Header.tsx
@@ -1,3 +1,4 @@
+import { header } from "motion/react-client";
 import { FC } from "react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
@@ -25,7 +26,35 @@ const navItems = [
 ];
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container !max-w-full">
+        <div className="flex justify-between h-20 items-center">
+          {/* for the logo */}
+          <div>
+            <a href="/">
+              <span className="text-xl font-bold uppercase">Subh</span>
+            </a>
+          </div>
+          {/* for the nav content */}
+          <div>
+            <div className="border border-stone-400 size-11 rounded-full inline-flex justify-center items-center">
+              <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect x="3" y="15" width="18" height="2" fill="currentColor" />
+              <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+            </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
