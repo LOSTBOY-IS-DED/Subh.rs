98c360cf39dca337612aaecc716adf48c701c6be
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-14 21:31:08 +0530

    testimonial for mobile done

--- starter-template/src/sections/Testimonials.tsx
+++ starter-template/src/sections/Testimonials.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import image1 from "@/assets/images/testimonial-1.jpg";
 import image2 from "@/assets/images/testimonial-2.jpg";
 import image3 from "@/assets/images/testimonial-3.jpg";
+import Image from "next/image";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const testimonials = [
@@ -35,7 +36,90 @@ const testimonials = [
 ];
 
 const Testimonials: FC = () => {
-  return <div>Testimonials</div>;
+
+  const testimonialIndex = 0 ; 
+
+
+  return (
+    <section className="section">
+      <h2 className="text-4xl flex flex-col overflow-hidden">
+        <span className="whitespace-nowrap">
+          Some Nice words form my Clients
+        </span>
+        <span className="whitespace-nowrap self-end text-red-orange-500">
+          Some Nice words form my Clients
+        </span>
+      </h2>
+
+      <div className="container">
+        <div className="mt-20">
+          {testimonials.map(
+            ({ name, company, role, image, quote, imagePositionY } , index) => index === testimonialIndex && (
+              <div key={name}>
+                <div className="aspect-square">
+                  <Image
+                    src={image}
+                    alt={name}
+                    className="size-full object-cover"
+                    style={{
+                      objectPosition: `50% ${imagePositionY * 100}%`,
+                    }}
+                  />
+                </div>{" "}
+                <div>
+                  <blockquote>
+                    <div className="text-3xl mt-8">
+                      <span>&ldquo;</span>
+                      <span>{quote}</span>
+                      <span>&rdquo;</span>
+                    </div>
+
+                    <cite className="mt-4 not-italic block">
+                      {name} , {role} at {company}
+                    </cite>
+                  </blockquote>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+        <div className="flex gap-4 mt-6">
+          <button className="border border-stone-400 size-11 inline-flex items-center justify-center rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </button>
+          <button className="border border-stone-400 size-11 inline-flex items-center justify-center rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
