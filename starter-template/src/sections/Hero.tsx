dc5c87912092f6bac2af12dcff37050e94d276e3
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-14 05:53:12 +0530

    image size fixed

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -9,43 +9,48 @@ const Hero: FC = () => {
     <section>
       <div className="grid md:grid-cols-12 md:h-screen items-stretch">
         <div className="col-span-7 flex flex-col justify-center">
-                <div className="container !max-w-full">
-        <h1 className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0">
-          I write code, I ship fast faster than your ex moved on. From stack to chain, I build empires... but don&apos;t worry, I&apos;ve still got time to ruin your sleep schedule too. 😘
-        </h1>
-        <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
-          <Button variant="secondary" iconAfter={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+          <div className="container !max-w-full">
+            <h1 className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0">
+              I write code, I ship fast faster than your ex moved on. From stack
+              to chain, I build empires... but don&apos;t worry, I&apos;ve still
+              got time to ruin your sleep schedule too. 😘
+            </h1>
+            <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
+              <Button
+                variant="secondary"
+                iconAfter={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                    />
+                  </svg>
+                }
+              >
+                <span>View My Work</span>
+              </Button>
+              <Button variant="text">Let&apos;s Talk</Button>
+            </div>
+          </div>
+        </div>
+        <div className="md:col-span-5">
+          <div className="mt-20 md:mt-0 md:h-full">
+            <Image
+              src={heroImage}
+              alt="My Profile picture"
+              className="size-full object-cover"
             />
-          </svg>
-        }>
-          <span>View My Work</span>
-          
-        </Button>
-        <Button variant="text">Let&apos;s Talk</Button>
-        </div>
-        
-      </div>
-        </div>
-        <div className="col-span-5">
-                <div className="mt-20 md:mt-0 md:h-full">
-        <Image src={heroImage} alt="My Profile picture" className="size-full object-cover" />
-      </div>
+          </div>
         </div>
       </div>
-
-
     </section>
   );
 };
