264bef8d9cec60dad4ed2905dcfd7b333d8bf421
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-15 21:15:44 +0530

    footer done responsive

--- starter-template/src/sections/Footer.tsx
+++ starter-template/src/sections/Footer.tsx
@@ -1,3 +1,4 @@
+
 import Button from "@/components/Button";
 import { FC } from "react";
 
@@ -24,7 +25,7 @@ const navItems = [
   },
 ];
 
-const Footer: FC = () => {
+const Footer : FC = () => {
   return (
     <footer className="bg-stone-900 text-white">
       <div className="container">
@@ -32,42 +33,49 @@ const Footer: FC = () => {
           <div className="flex gap-3 items-center">
             <div className="size-3 bg-green-400 rounded-full"></div>
             <span className="uppercase">
-              {" "}
-              One Stop Available for next month{" "}
+              One Stop Available for next month
             </span>
           </div>
-          <h2 className="text-4xl mt-8 font-extralight">
-            Enough talk let&apos;s make something great together.
-          </h2>
-          <Button
-            className="mt-8"
-            variant="secondary"
-            iconAfter={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-6"
+          <div className="grid md:grid-cols-3 gap-8">
+            <div className="md:col-span-2">
+              <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight">
+                Enough talk let&apos;s make something great together.
+              </h2>
+              <Button
+                className="mt-8"
+                variant="secondary"
+                iconAfter={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>
+                }
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                />
-              </svg>
-            }
-          >
-            info@subh.com
-          </Button>
-          <nav className="flex flex-col gap-8 mt-16 ">
-            {navItems.map(({ href, label }) => (
-              <a href={href} key={label}>
-                <Button variant="text" className="text-lg">{label}</Button>
-              </a>
-            ))}
-          </nav>
+                info@subh.com
+              </Button>
+            </div>
+            <div className="md:col-span-1">
+              <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
+                {navItems.map(({ href, label }) => (
+                  <a href={href} key={label}>
+                    <Button variant="text" className="text-lg">
+                      {label}
+                    </Button>
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
         </div>
         <p className="py-16 text-white/30 text-sm">
           Copyright &copy; Subh &bull; All rights reserved.
